df893b5c67a7ba86123d6127cefeddd1dbe6f506
matteo125 <matteo.revelli125@gmail.com>
2016-03-04 17:39:51 +0100

    improve code

--- app/controllers/ecard_controller.rb
+++ app/controllers/ecard_controller.rb
@@ -1,99 +1,15 @@
 class EcardController < ApplicationController
+  def new
+	@ecard = Ecard.new
+  end
 
-	def new
-		@ecard = Ecard.new
-	end
+  def create
+    @ecard = Ecard.new(params[:ecard])
+	@ecard.generate
+    send_data @ecard.content, type: "plain/text", filename: "ecard-#{params[:ecard][:brand]}-#{params[:ecard][:lang]}.html"
+  end
 
-	def create
-	    @ecard = Ecard.new(
-	    	params[:ecard][:brand],
-	    	params[:ecard][:lang],
-	    	params[:ecard][:title], 
-	    	params[:ecard][:pre_header],
-	    	params[:ecard][:pre_header_sel], 
-	    	params[:ecard][:width],
-	    	params[:ecard][:height],
-	    	params[:ecard][:src],
-	    	params[:ecard][:link],
-	    	params[:ecard][:alt],
-	    	params[:ecard][:forward])
-
-#SCELTA BRAND
-	    if params[:ecard][:brand] == 'MiuMiu'
-	    	data = File.read("config/template/ecard_template_miumiu.html")
-	    else
-	    	data = File.read("config/template/ecard_template_prada.html")
-		end
-
-#DATI
-	    @content = data
-		    @content.gsub!("titolo", params[:ecard][:title])
-
-		    if params[:ecard][:pre_header_sel] == 'No'
-			    @content.gsub!("#p_h_yes_1", "<!--")
-			    @content.gsub!("#p_h_yes_2", "-->")
-			else
-				@content.gsub!("#p_h_yes_1", "")
-			    @content.gsub!("#p_h_yes_2", "")
-			end
-
-			@content.gsub!("pre_header", params[:ecard][:pre_header])
-		    @content.gsub!("img_width", params[:ecard][:width])
-		    @content.gsub!("img_height", params[:ecard][:height])
-		    @content.gsub!("#src", params[:ecard][:src])
-
-		    if params[:ecard][:link_sel] == 'No'
-			    @content.gsub!("#l_no_1", "<!--")
-			    @content.gsub!("#l_no_2", "-->")
-			else
-				@content.gsub!("#l_no_1", "")
-			    @content.gsub!("#l_no_2", "")
-			end
-
-		    @content.gsub!("link", params[:ecard][:link])
-		    @content.gsub!("#alt", params[:ecard][:alt])
-
-		    if params[:ecard][:forward_sel] == 'No'
-			    @content.gsub!("#f_no_1", "<!--")
-			    @content.gsub!("#f_no_2", "-->")
-			else
-				@content.gsub!("#f_no_1", "")
-			    @content.gsub!("#f_no_2", "")	
-			end
-
-#TRADUZIONI
-		require 'yaml'
-		
-#prada
-		if params[:ecard][:brand] == "Prada"
-			translate = YAML.load_file("config/locales/translate_prada.yml")
-			brand = "PRADA"
-		else
-			translate = YAML.load_file("config/locales/translate_miumiu.yml")
-			brand = "MIU MIU"
-		end
-		
-		@content.gsub!("If you have trouble seeing this e-mail please ", translate[params[:ecard][:lang]]["browser_view_1"])
-		@content.gsub!("click here</a> to unsubscribe. <br />", translate[params[:ecard][:lang]]["footer_3"])
-		@content.gsub!("click here", translate[params[:ecard][:lang]]["browser_view_2"])
-		@content.gsub!("To ensure that our communications are safely received in your inbox,<br/>please add ", translate[params[:ecard][:lang]]["footer_1"])
-		@content.gsub!("to your address book.", translate[params[:ecard][:lang]]["footer_1_2"])
-		@content.gsub!("Forward to a friend", translate[params[:ecard][:lang]]["forward"])
-		@content.gsub!("We legitimately collected your e-mail address in circumstances such as in stores or through the web site,<br />when you knowingly provided this information. We make our best effort in order to handle such information<br />in conformity with privacy regulations and in complete safety. Please ", translate[params[:ecard][:lang]]["footer_2"])
-		@content.gsub!("Click here</a> for information related to #{brand} company and legal information.", translate[params[:ecard][:lang]]["legal_informations"])
-
-
-#DOWNLOAD
-		send_data @content, type: "plain/text", filename: "ecard-#{params[:ecard][:brand]}-#{params[:ecard][:lang]}.html"
-	end
-
-	def show
-	    @content
-	end
-
-
-	def download
-		send_data @content, type: "plain/text", filename: "ecard#{params[:ecard][:lang]}.html"
-	end
-
+  def show
+    @content
+  end
 end
--- app/models/ecard.rb
+++ app/models/ecard.rb
@@ -1,34 +1,97 @@
 class Ecard
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
 
-	extend ActiveModel::Naming
-  	include ActiveModel::Conversion
+  attr_accessor :content, :brand, :lang, :forward, :forward_sel, :width, :height, :pre_header_sel, :link_sel, :title, :pre_header, :src, :link, :alt
+  BRANDS = { "prada": "PRADA", "miumiu": "MIU MIU" }
 
-  	def persisted?
-    	false
-  	end
-
-	attr_accessor :brand, :lang, :title, :pre_header, :pre_header_sel, :width, :height, :src, :link, :link_sel, :alt, :forward_sel
-
-	def initialize brand = "", lang = "", title = "", pre_header = "", pre_header_sel = "", width = "", height = "", src = "", link = "", link_sel = "", alt = "", forward_sel = ""
-    	@brand = brand
-      @lang = lang
-    	@title = title
-    	@pre_header = pre_header
-      @pre_header_sel = pre_header_sel
-    	@width = width
-    	@height = height
-    	@src = src
-    	@link = link
-      @link_sel = link_sel
-    	@alt = alt
-    	@forward_sel = forward_sel
+	def persisted?
+  	false
 	end
 
-	def load_template
-
-		#@content = file.read
+	def initialize options = {}
+    options[:brand] ||= "prada"
+  	@options = options
+    brand_param = options[:brand].downcase
+    @content = File.read("config/template/ecard_template_#{brand_param}.html")
+    @translate = YAML.load_file("config/locales/translate_#{brand_param}.yml")
+    @brand = BRANDS[brand_param.to_sym]
 	end
 
+  def generate
+    sub_title
+    select_header   
+    sub_pre_header
+    images_dimension
+    set_src
+    select_link
+    set_images_attributes    
+    select_forward
+    sub_translations
+  end
 
+  private
 
+  def sub_title
+     @content.gsub!("titolo", @options[:title])
+  end
+
+  def select_header
+    if @options[:pre_header_sel] == 'No'
+      @content.gsub!("#p_h_yes_1", "<!--")
+      @content.gsub!("#p_h_yes_2", "-->")
+    else
+      @content.gsub!("#p_h_yes_1", "")
+      @content.gsub!("#p_h_yes_2", "")
+    end
+  end
+
+  def sub_pre_header
+    @content.gsub!("pre_header", @options[:pre_header])
+  end
+
+  def images_dimension
+    @content.gsub!("img_width", @options[:width])
+    @content.gsub!("img_height", @options[:height])
+  end
+
+  def set_src
+    @content.gsub!("#src", @options[:src])
+  end
+
+  def select_link
+    if @options[:link_sel] == 'No'
+      @content.gsub!("#l_no_1", "<!--")
+      @content.gsub!("#l_no_2", "-->")
+    else
+      @content.gsub!("#l_no_1", "")
+      @content.gsub!("#l_no_2", "")
+    end
+  end
+
+  def set_images_attributes
+    @content.gsub!("link", @options[:link])
+    @content.gsub!("#alt", @options[:alt])
+  end
+
+  def sub_translations
+    @content.gsub!("If you have trouble seeing this e-mail please ", @translate[@options[:lang]]["browser_view_1"])
+    @content.gsub!("click here</a> to unsubscribe. <br />", @translate[@options[:lang]]["footer_3"])
+    @content.gsub!("click here", @translate[@options[:lang]]["browser_view_2"])
+    @content.gsub!("To ensure that our communications are safely received in your inbox,<br/>please add ", @translate[@options[:lang]]["footer_1"])
+    @content.gsub!("to your address book.", @translate[@options[:lang]]["footer_1_2"])
+    @content.gsub!("Forward to a friend", @translate[@options[:lang]]["forward"])
+    @content.gsub!("We legitimately collected your e-mail address in circumstances such as in stores or through the web site,<br />when you knowingly provided this information. We make our best effort in order to handle such information<br />in conformity with privacy regulations and in complete safety. Please ", @translate[@options[:lang]]["footer_2"])
+    @content.gsub!("Click here</a> for information related to #{brand} company and legal information.", @translate[@options[:lang]]["legal_informations"])
+  end
+
+  def select_forward
+    if @options[:forward_sel] == 'No'
+      @content.gsub!("#f_no_1", "<!--")
+      @content.gsub!("#f_no_2", "-->")
+    else
+      @content.gsub!("#f_no_1", "")
+      @content.gsub!("#f_no_2", "") 
+    end
+  end
 end
