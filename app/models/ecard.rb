5c07089f599d4cf0e70bce1f9fc71302674b8d39
matteo125 <matteo.revelli125@gmail.com>
2016-03-14 15:44:45 +0100

    add translations and new partials

--- app/models/ecard.rb
+++ app/models/ecard.rb
@@ -3,7 +3,6 @@ class Ecard
   include ActiveModel::Conversion
 
   attr_accessor :content, :brand, :brand_downcase, :lang, :forward, :forward_sel, :width, :height, :pre_header_sel, :link_sel, :title, :pre_header, :src, :link, :alt
-  BRANDS = { "prada": "PRADA", "miumiu": "MIU MIU" }
 
 	def persisted?
   	false
@@ -19,16 +18,17 @@ class Ecard
     @alt = options[:alt]
     @pre_header_sel = options[:pre_header_sel]
     @forward_sel = options[:forward_sel]
-
-    @brand_downcase = options[:brand].downcase ||= "prada"
-    
+    @brand = options[:brand] ||= "prada"
     brand_param = options[:brand].downcase
-    @brand = BRANDS[brand_param.to_sym]
 	end
   
   def content
     template = ERB::new File.read("config/templates/template.html.erb")
     pre_header_template = ERB::new(File.read("config/templates/_pre_header.html.erb"),0,"","_erbout2").result binding
+    link_template = ERB::new(File.read("config/templates/_link.html.erb"),0,"","_erbout2").result binding
+    social_template = ERB::new(File.read("config/templates/_social.html.erb"),0,"","_erbout2").result binding
+    forward_template = ERB::new(File.read("config/templates/_forward.html.erb"),0,"","_erbout2").result binding
+    footer_template = ERB::new(File.read("config/templates/_footer.html.erb"),0,"","_erbout2").result binding
     template.result binding
   end
 end
