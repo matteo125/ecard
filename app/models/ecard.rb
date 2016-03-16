63a04761831c1b507d6e4c255161509b34de925f
matteo125 <matteo.revelli125@gmail.com>
2016-03-16 17:13:40 +0100

    change partial and templates

--- app/models/ecard.rb
+++ app/models/ecard.rb
@@ -25,9 +25,7 @@ class Ecard
     template = ERB::new File.read("config/templates/template_#{@brand}.html.erb")
     pre_header_template = ERB::new(File.read("config/templates/_pre_header.html.erb")).result binding
     link_template = ERB::new(File.read("config/templates/_link.html.erb")).result binding
-    social_template = ERB::new(File.read("config/templates/_social.html.erb")).result binding
     forward_template = ERB::new(File.read("config/templates/_forward.html.erb")).result binding
-    footer_template = ERB::new(File.read("config/templates/_footer.html.erb")).result binding
     template.result binding
   end
 
