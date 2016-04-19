cddff87fb5913facf381ac4a14e4482d4b48d8e7
matteo125 <matteo.revelli125@gmail.com>
2016-04-19 18:03:23 +0200

    change french translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,6 @@ class ApplicationController < ActionController::Base
 	I18n.locale = params[:locale] || I18n.default_locale
   end
 
-#  def default_url_options(options = {})
-#    { locale: I18n.locale }.merge options
-#  end
-
   def default_url_options
   	{ locale: I18n.locale }
   end
